4f487967c36a6792f83c7ce78e24511397842632
= <naujforero@gmail.com>
2024-09-06 16:24:12 -0500

    Se corrigio la salida del nombre del gráfico de barras en en el main.py y se agregaron las gráficas para el caso de Colombia, Peru y Bolivia

--- app/charts.py
+++ app/charts.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 
-def generate_bar_chart(labels, values):
+def generate_bar_chart(country, labels, values):
   fig, ax = plt.subplots()
   ax.bar(labels, values)
-  plt.savefig('bar.png')
+  plt.savefig(f'./imgs/{country}.png')
   plt.close()
 
 def generate_pie_chart(labels, values):
